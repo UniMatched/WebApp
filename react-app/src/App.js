fdea052154f0b3fa162ddd24f6f599cd45dde893
Lorenzo Rota <lorenzo.rota@hotmail.com>
2020-10-14 12:13:30 +0200

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return(
+  <div className="ReactApp">
+    <header className="header">
+      <h1 className="title">UniMatched!</h1>
+    </header>
+    <p className="UniMatched landing page">
+      Welcome to the landing page of UniMatched
+    </p>
+  </div>
+  );
+}
+
+export default App
