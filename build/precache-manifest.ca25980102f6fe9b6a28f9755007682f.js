5f960aea9fb009fcd3857d08aea53f8d5e5a6f8d
Lorenzo Rota <lorenzo.rota@hotmail.com>
2020-10-14 13:06:26 +0200

    the react-app is now the parent directory

---
+++ build/precache-manifest.ca25980102f6fe9b6a28f9755007682f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fb84aa43c7761fdc08e209d8f1ea2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a31cb11db79d18b64735",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b8dc912edce1cb3e0ff",
+    "url": "/static/js/2.3e2e52e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e2e52e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31cb11db79d18b64735",
+    "url": "/static/js/main.a0e89540.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
